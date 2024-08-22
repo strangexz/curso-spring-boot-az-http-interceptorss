6fcb6f40217b1e52d58d30e9f68c91f6468c34aa
Strangexz <ja_figueroa@outlook.es>
2024-08-22 15:09:00 -0600

    refactor: enviando un false en el interceptor

--- src/main/java/hn/com/jf/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/hn/com/jf/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,8 @@
 package hn.com.jf.interceptors;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.slf4j.Logger;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -32,7 +37,17 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
 		int delay = random.nextInt(500);
 		Thread.sleep(delay);
 		
-		return HandlerInterceptor.super.preHandle(request, response, handler);
+		Map<String, String> json = new HashMap<>();
+		json.put("error", "No tienes acceso a este recurso.");
+		json.put("date", new Date().toString());
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(json);
+		response.setContentType("application/json");
+		response.setStatus(401);
+		response.getWriter().write(jsonString);
+		
+//		return HandlerInterceptor.super.preHandle(request, response, handler);
+		return false;
 	}
 
 	@Override
